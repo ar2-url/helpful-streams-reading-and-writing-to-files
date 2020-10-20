47cafddc17c640e1f92787851fa5690e0e7d78f4
ar2-url <57313716+ar2-url@users.noreply.github.com>
2020-10-20 20:39:20 +0100

    Add files via upload

---
+++ app.js
@@ -0,0 +1,16 @@
+const readline = require('readline');
+const fs = require('fs');
+
+let settings = {
+  input: fs.createReadStream('shoppingList.txt')
+};
+
+const myInterface = readline.createInterface(settings);
+
+const printData = (data) => {
+  console.log(`Item: ${data}`);
+};
+
+myInterface.on('line', printData);
+
+//output after: node app.js
